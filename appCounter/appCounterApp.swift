06a80f8e54d312ce237b6fdbbb8c95e870a48ec2
Luis Subiabre <luis.subiabre@gmail.com>
2022-01-26 20:34:36 -0300

    Initial Commit

---
+++ appCounter/appCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  appCounterApp.swift
+//  appCounter
+//
+//  Created by Luis Subiabre Salviat on 26-01-22.
+//
+
+import SwiftUI
+
+@main
+struct appCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
